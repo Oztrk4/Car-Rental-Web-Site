dc1ffa38dd624461d175108cae990b951aafc6f1
Oztrk4 <oztrklr2010@gmail.com>
2024-02-07 22:15:42 +0300

    Car Rental Web Site
    Car Rental web site with HTML & CSS

---
+++ Web2/JavaScript/cars.js
@@ -0,0 +1,22 @@
+
+var MainImage = document.getElementById("MainImage");
+var SmallImage = document.getElementsByClassName("small-image");
+
+SmallImage[0].onclick = function() {
+  MainImage.src = SmallImage[0].src;
+}
+SmallImage[1].onclick = function() {
+  MainImage.src = SmallImage[1].src;
+}
+SmallImage[2].onclick = function() {
+  MainImage.src = SmallImage[2].src;
+}
+SmallImage[3].onclick = function() {
+  MainImage.src = SmallImage[3].src;
+}
+SmallImage[4].onclick = function() {
+  MainImage.src = SmallImage[4].src;
+}
+SmallImage[5].onclick = function() {
+  MainImage.src = SmallImage[5].src;
+}
---
+++ Web2/JavaScript/review.js
@@ -0,0 +1,6 @@
+function myFunction() {
+  var x = document.getElementById("myInput").value;
+  var y = document.getElementById("message").value;
+  document.getElementById("myH").innerHTML = " " + x;
+  document.getElementById("myP").innerHTML = " " + y;
+}
